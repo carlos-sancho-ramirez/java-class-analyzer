745860c6f72b7fe01f537d1476c416f937bdd5d3
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2013-12-22 15:15:38 +0100

    Updated test for the java_type package

--- src/test/scala/sword/java/class_analyzer/java_type/JavaTypeTest.scala
+++ src/test/scala/sword/java/class_analyzer/java_type/JavaTypeTest.scala
@@ -1,74 +1,72 @@
 package sword.java.class_analyzer.java_type
 
 import org.scalatest.FunSuite
+import sword.java.class_analyzer.independent_type.IndependentTypeTest
+import sword.java.class_analyzer.ref.RootReference
 
 class JavaTypeTest extends FunSuite {
 
-  val primitiveSignatures = "V" :: "Z" :: "B" :: "C" :: "S" :: "I" :: "J" :: "F" :: "D" :: List()
-  val arraySignatures = primitiveSignatures.map( "[" + _ )
+  val primitiveSignatures = IndependentTypeTest.primitiveSignatures
+  val arraySignatures = IndependentTypeTest.arraySignatures
   val someClassRef = "Ljava/util/List;" :: "Ljava/lang/Class;" :: "Lorg/scalatest/FunSuite;" :: List()
   val wrongClassRefs = null :: "" :: "L;" :: "Ljava/util/List" :: "java/util/List;" :: "Java/util/List" :: "List" :: "Ljava.util.List;" :: List()
 
-  val validTypeLists = "[ZI" :: "D[[B" :: "ZLjava/lang/String;[I" :: "[Ljava/util/List;D" :: List()
+  val typeLists: List[List[String]] =
+    ("[Z" :: "I" :: List()) ::
+    ("D" :: "[[B" :: List()) ::
+    ("Z" :: "Ljava/lang/String;" :: "[I" :: List()) ::
+    List()
+  //val validTypeLists = "[ZI" :: "D[[B" :: "ZLjava/lang/String;[I" :: "[Ljava/util/List;D" :: List()
+  val validTypeLists = typeLists.map( x => x.foldLeft("")(_ + _))
   val validMethodSignatures = validTypeLists.map(x => "(" + x + ")I") ::: ("()V" :: List())
 
   val allValidValues = primitiveSignatures ::: arraySignatures ::: someClassRef ::: validTypeLists ::: validMethodSignatures
   val allTestingValues = wrongClassRefs ::: allValidValues
 
+  val someWrongValues = "X" :: "S[" :: List()
+  val allWrongValues = wrongClassRefs ::: someWrongValues
+
   test("Valid values for signatures never gives null references") {
-    allValidValues foreach { x => assert( JavaType.getFromSignature(x) ne null, "null reference for " + x) }
+    val factory = new ExtendedTypeFactory(new RootReference)
+    allValidValues foreach { x => assert( factory.getFromSignature(x) ne null, "null reference for " + x) }
   }
 
   test("Invalid signatures returns null references") {
-    wrongClassRefs foreach { x => assert( JavaType.getFromSignature(x) eq null, "not null reference for " + x) }
+    val factory = new ExtendedTypeFactory(new RootReference)
+    allWrongValues foreach { x => assert( factory.getFromSignature(x) eq null, "not null reference for " + x) }
   }
 
   test("Signature is the one given") {
+    val factory = new ExtendedTypeFactory(new RootReference)
     allValidValues foreach { x =>
-      assert(JavaType.getFromSignature(x).signature() === x)
+      assert(factory.getFromSignature(x).signature === x)
     }
   }
 
-  test("Same instance") {
-    allTestingValues.foreach { x =>
-      val a = JavaType getFromSignature x
-      val b = JavaType getFromSignature x
-      assert(a eq b, s"Calling getFromSignature for $x do not always returns the same instance. " + (if (a != null) a.signature else "null"))
-    }
-  }
-
-  test("different instances") {
-    allValidValues foreach {
-      x => allValidValues.foreach {
-        y => if (x != y) {
-          val a = JavaType getFromSignature x
-          val b = JavaType getFromSignature y
-          assert(a ne b)
-        }
+  test("JavaTypeList.toArray return the expected values") {
+    val factory = new ExtendedTypeFactory(new RootReference)
+    var lists = typeLists
+    validTypeLists foreach { x =>
+      val list = factory.getFromSignature(x).asInstanceOf[JavaTypeList]
+      var expectedList = lists.head
+      list.toArray() foreach { element =>
+        assert(element.signature === expectedList.head)
+        expectedList = expectedList.tail
       }
+
+      lists = lists.tail
     }
   }
 
-  test("only JavaTypeList return true on isTypeList method") {
-    allValidValues foreach { x =>
-      val isList = validTypeLists.contains(x)
-      val javaTypeList = JavaType.getFromSignature(x)
-      assert(javaTypeList.isTypeList() == isList, "isList returns" + !isList + " for " + x)
-    }
-  }
-
-  test("only JavaMethod returns a non null instance on tryCastingToMethod") {
-    allValidValues foreach { x =>
-      val isMethod = validMethodSignatures.contains(x)
-      val javaType = JavaType.getFromSignature(x)
-      if (isMethod) {
-        val method = javaType.tryCastingToMethod
-        assert((method ne null) && method.isInstanceOf[JavaMethod])
-      }
-      else {
-        val method = javaType.tryCastingToMethod
-        assert(method eq null)
-      }
+  test("JavaMethod includes the expected list and returning value") {
+    val factory = new ExtendedTypeFactory(new RootReference)
+    validMethodSignatures foreach { signature =>
+      val splitPosition = signature.lastIndexOf(")");
+      val paramsSignature = signature.substring(1, splitPosition)
+      val retSignature = signature.substring(splitPosition + 1)
+      val method = factory.getFromSignature(signature).asInstanceOf[JavaMethod]
+      assert(method.getParameterTypeList.signature === paramsSignature)
+      assert(method.getReturningType.signature === retSignature)
     }
   }
 }
